772d7b19bb9b4172ee28c35f6cb898b2c8196bd4
hanspam <hans.ajani@willis.com>
2022-03-15 16:16:35 -0400

    Add files via upload

---
+++ Services/Services.Queries/PostQueryService.cs
@@ -0,0 +1,34 @@
+﻿using Common.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Database;
+using Services.Queries.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Queries
+{
+    public class PostQueryService : IPostQueryService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public PostQueryService(ApplicationDbContext applicationDbContext) 
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<List<PostDto>> GetAllAsync() 
+        {
+            var collection = await _applicationDbContext.Posts.ToListAsync();
+
+            return collection.MapTo<List<PostDto>>();
+        }
+
+        public async Task<PostDto> GetAsync(int id)
+        {
+            return (await _applicationDbContext.Posts.SingleAsync(x => x.PostId == id)).MapTo<PostDto>();
+        }
+    }
+}
---
+++ Services/Services.Queries/UserQueryService.cs
@@ -0,0 +1,34 @@
+﻿using Common.Mapping;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Database;
+using Services.Queries.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Queries
+{
+    public class UserQueryService : IUserQueryService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UserQueryService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<List<UserDto>> GetAllAsync()
+        {
+            var collection = await _applicationDbContext.Users.ToListAsync();
+
+            return collection.MapTo<List<UserDto>>();
+        }
+
+        public async Task<UserDto> GetAsync(int id)
+        {
+            return (await _applicationDbContext.Users.SingleAsync(x => x.UserId == id)).MapTo<UserDto>();
+        }
+    }
+}
