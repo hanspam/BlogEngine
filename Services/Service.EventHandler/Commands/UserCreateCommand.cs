4c219f658edc31b32a4e74d4c7298346db4fe86b
hanspam <hans.ajani@willis.com>
2022-03-15 15:52:51 -0400

    Add files via upload

---
+++ Services/Service.EventHandler/Commands/UserCreateCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.EventHandler.Commands
+{
+    public class UserCreateCommand : INotification
+    {
+        public int UserId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string? UserRol { get; set; }
+    }
+}
---
+++ Services/Service.EventHandler/CommentCreateEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Models.Domain;
+using Persistence.Database;
+using Service.EventHandler.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.EventHandler
+{
+    public class CommentCreateEventHandler : INotificationHandler<CommentCreateCommand>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CommentCreateEventHandler(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Handle(CommentCreateCommand command, CancellationToken cancellationToken)
+        {
+            await _applicationDbContext.AddAsync(new Comment
+            {
+                CommentId = command.CommentId,
+                User = command.User,
+                Post = command.Post,
+                Annotation = command.Annotation,
+            });
+
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
