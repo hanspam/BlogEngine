bc32e26e82b34232e0233bb0fe5a9b068609134f
hanspam <hans.ajani@willis.com>
2022-03-15 15:53:36 -0400

    Add files via upload

---
+++ Services.Queries/DTOs/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Queries.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string? UserRol { get; set; }
+    }
+}
