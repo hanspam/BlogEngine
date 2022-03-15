917c8f5f08b570dc6b87e247c1ec671c6f3fd56a
hanspam <hans.ajani@willis.com>
2022-03-15 16:14:31 -0400

    Add files via upload

---
+++ Services/Service.EventHandler/PostCreateEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Models.Domain;
+using Persistence.Database;
+using Service.EventHandler.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.EventHandler
+{
+    public class PostCreateEventHandler : INotificationHandler<PostCreateCommand>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public PostCreateEventHandler(ApplicationDbContext applicationDbContext) 
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Handle(PostCreateCommand command, CancellationToken cancellationToken) 
+        {
+            await _applicationDbContext.AddAsync(new Post
+            {
+                Content = command.Content,
+                User = command.UserId,
+                Created = command.Created,
+                Title = command.Title,
+                Status = command.StatusId,
+            });
+
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Services/Service.EventHandler/UserCreateEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Models.Domain;
+using Persistence.Database;
+using Service.EventHandler.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.EventHandler
+{
+    public class UserCreateEventHandler : INotificationHandler<UserCreateCommand>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UserCreateEventHandler(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task Handle(UserCreateCommand command, CancellationToken cancellationToken)
+        {
+            await _applicationDbContext.AddAsync(new User
+            {
+                UserId = command.UserId,
+                Name = command.Name,
+                LastName = command.LastName,
+                UserRol = command.UserRol,
+                CreatedDate = command.CreatedDate,
+            });
+
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
