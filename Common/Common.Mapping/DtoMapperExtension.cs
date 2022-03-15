f1be358b848c20276fb83450ae08fba75d336c1c
hanspam <hans.ajani@willis.com>
2022-03-15 15:48:20 -0400

    Add files via upload

---
+++ Common/Common.Mapping/DtoMapperExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Common.Mapping
+{
+    public static class DtoMapperExtension
+    {
+        public static T MapTo<T>(this object value)
+        {
+            return JsonSerializer.Deserialize<T>(
+                JsonSerializer.Serialize(value)
+            );
+        }
+    }
+}
