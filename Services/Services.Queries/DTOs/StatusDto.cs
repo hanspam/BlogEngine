cbe0d5e0eafbdcb3836630513301abf10220cf3d
hanspam <hans.ajani@willis.com>
2022-03-15 16:00:07 -0400

    Add files via upload

---
+++ Services/Services.Queries/DTOs/StatusDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Queries.DTOs
+{
+    public class StatusDto
+    {
+        public int StatusId { get; set; }
+
+        public string? StatusName { get; set; }
+    }
+}
---
+++ Services/Services.Queries/ICommentQueryService.cs
@@ -0,0 +1,16 @@
+﻿using Services.Queries.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Queries
+{
+    public interface ICommentQueryService
+    {
+        Task<List<CommentDto>> GetAllAsync();
+
+        Task<CommentDto> GetAsync(int id);
+    }
+}
